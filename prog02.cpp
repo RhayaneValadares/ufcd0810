8c7fc6692e0948d04750df52c604ec5ab90b2a56
RhayaneValadares <rhayane.jose@outlook.com>
2024-12-02 17:04:09 +0000

    prog02
    exercio02 aula 02/12

---
+++ prog02.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int idade;
+    float Altura;
+    cout<<"Digite Sua Idade: ";
+      cin>>idade;
+    cout<<"Digite Sua altura: ";
+      cin>>Altura;
+    cout<<"SUA IDADE EH: "<<idade<<"e altura é "<< Altura <<endl;
+    return 0;
+}
